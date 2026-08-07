ca7252cd34545265449fcd3da3fa3bf9c32181b8
VicZack Plugins <obeliscorp.network@gmail.com>
2026-08-06 23:55:57 -0400

    feat: Add sub to events

--- UGroupsCount/UGroupsCountConfiguration.cs
+++ UGroupsCount/UGroupsCountConfiguration.cs
@@ -1,12 +1,44 @@
 ﻿using Rocket.API;
+using System.Collections.Generic;
 
 namespace VicZackPlugins.UGroupsCount
 {
     public class UGroupsCountConfiguration : IRocketPluginConfiguration
     {
+        // Show the name of group
+        public bool showName { get; set; }
+
+        // Show de count of group
+        public bool showCount { get; set; }
+
+        // Show group list
+        public List<GroupSlotConfig> Slots { get; set; }
+
         public void LoadDefaults()
         {
 
+            showName = true;
+            showCount = true;
+
+            Slots = new List<GroupSlotConfig>
+            {
+                new GroupSlotConfig { SlotIndex = 0, DisplayName = "Grupo 1", Icon = 100, GroupId = "grupo1" },
+                new GroupSlotConfig { SlotIndex = 1, DisplayName = "Grupo 2", Icon = 101, GroupId = "grupo2" },
+                new GroupSlotConfig { SlotIndex = 2, DisplayName = "Grupo 3", Icon = 102, GroupId = "grupo3" },
+                new GroupSlotConfig { SlotIndex = 3, DisplayName = "Grupo 4", Icon = 103, GroupId = "grupo4" },
+                new GroupSlotConfig { SlotIndex = 4, DisplayName = "Grupo 5", Icon = 104, GroupId = "grupo5" },
+                new GroupSlotConfig { SlotIndex = 5, DisplayName = "Grupo 6", Icon = 105, GroupId = "grupo6" },
+            };
+
         }
+    }
+
+    public class GroupSlotConfig
+    {
+        public int SlotIndex { get; set; }
+        public string DisplayName { get; set; }
+        public ushort Icon { get; set; }
+        public string GroupId { get; set; }
+        public bool Enabled { get; set; } = true;
     }
 }
--- UGroupsCount/UGroupsCountPlugin.cs
+++ UGroupsCount/UGroupsCountPlugin.cs
@@ -1,18 +1,38 @@
 ﻿using Rocket.Core.Logging;
 using Rocket.Core.Plugins;
+using Rocket.Unturned;
+using Rocket.Unturned.Events;
 
 namespace VicZackPlugins.UGroupsCount
 {
     public class UGroupsCountPlugin : RocketPlugin<UGroupsCountConfiguration>
     {
+        public string PLUGIN_VERSION => "1.0.0";
         protected override void Load()
         {
-            Logger.Log($"{Name} {Assembly.GetName().Version.ToString(3)} has been loaded!");
+            Logger.Log(" ");
+            Logger.Log("---------------------------------------");
+            Logger.Log("------[VZ_UGroups Count Plugin]--------");
+            Logger.Log("--------[Successfully Loaded]----------");
+            Logger.Log("-----------[By: Vic Zack]--------------");
+            Logger.Log("--------[Discord: elbistec01]----------");
+            Logger.Log($"----------[Version: {PLUGIN_VERSION}]-------------");
+            Logger.Log("---------------------------------------");
+            Logger.Log(" ");
+
+            U.Events.OnPlayerConnected += ;
+            U.Events.OnPlayerDisconnected += ;
+
         }
 
         protected override void Unload()
         {
-            Logger.Log($"{Name} has been unloaded!");
+            U.Events.OnPlayerConnected -= ;
+            U.Events.OnPlayerDisconnected -= ;
+
+            Logger.Log("[UGC] Plugin unloaded.");
         }
+
+
     }
 }
